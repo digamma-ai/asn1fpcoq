48667b25e91a15e936a7916e039ea8dd6353f0b0
Ilia Zaichuk <zoickx@gmail.com>
2018-08-22 15:59:49 +0300

    Improve bit count positivity lemmas
    Atomize statements, simplify proofs slightly.

--- ASNProp.v
+++ ASNProp.v
@@ -21,22 +21,39 @@ Proof.
     + apply gt_Sn_O.
 Qed.
 
-Lemma gt_octets_O (p : positive) :
-  octets p > 0.
+Check Nat.divmod_spec (8 + 7) 7 0 7.
+
+Lemma gt_bitstooctets_O (b : nat) :
+  b > 0 -> bits_to_octets b > 0.
 Proof.
-  unfold octets, bits_to_octets.
-  assert(B: bits p  > 0) by apply gt_bits_O.
-  Opaque bits. simpl.
-  generalize (Nat.divmod_spec (bits p+7) 7 0 7).
-  intros H.
-  assert(S: 7<=7) by auto.
-  specialize (H S). clear S.
-  destruct (Nat.divmod (bits p + 7) 7 0 7).
-  destruct H as [H1 H2].
+  intros H1.
+  unfold bits_to_octets. simpl.
+  generalize (Nat.divmod_spec (b + 7) 7 0 7).
+  intros H2.
+  assert (H3: 7<=7) by reflexivity.
+  apply H2 in H3. clear H2.
+  destruct (Nat.divmod (b + 7) 7 0 7) as (q',u').
+  destruct H3 as [H4 H5].
   simpl.
   lia.
 Qed.
 
-Lemma twos_octets_correct (z : Z) :
-  twos_bits z <= 8 * (twos_octets z) < twos_bits z + 8.
+Lemma gt_octets_O (p : positive) :
+  octets p > 0.
+Proof.
+  unfold octets.
+  apply gt_bitstooctets_O.
+  apply gt_bits_O.
+Qed.
+
+Lemma gt_twosoctets_O (z : Z) :
+  twos_octets z > 0.
+Proof.
+  unfold twos_octets.
+  apply gt_bitstooctets_O.
+  apply gt_twosbits_O.
+Qed.
+
+Lemma bits_to_octets_correct (b : nat) :
+  b <= 8 * bits_to_octets b < b + 8.
 Admitted.
