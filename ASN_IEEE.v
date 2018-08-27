db3bbfcc711dd61ae5f0369a52830b19389ceca1
Vadim Zaliva <lord@crocodile.org>
2018-08-27 14:30:56 -0700

    high-level interface of converters

--- ASN_IEEE.v
+++ ASN_IEEE.v
@@ -1,48 +1,56 @@
+
 Require Import ZArith Datatypes.
 Require Import Flocq.IEEE754.Binary Flocq.IEEE754.Bits Flocq.Core.Zaux.
 Require Import ASNDef.
 
-Inductive convertible_IEEE :=
-  | Bin32 (f : binary32)
-  | Bin64 (f : binary64).
-
-Lemma bin32_finite_convertible (m : positive) (e : Z) :
-  Binary.bounded 24 128 m e = true -> good_real m e radix2 = true.
+Definition good_real_s (m : positive) (e : Z) (b : radix):
+  {good_real m e b = true} + {good_real m e b = false}.
 Admitted.
 
-Lemma bin64_finite_convertible (m : positive) (e : Z) :
-  Binary.bounded 53 1024 m e = true -> good_real m e radix2 = true.
+Definition bbounded_s (prec emax: Z) (m: positive) (e:Z):
+  {Binary.bounded prec emax m e = true}+{Binary.bounded prec emax m e = false}.
 Admitted.
 
-Definition IEEE_to_ASN (f : convertible_IEEE) : ASN_real :=
+Definition IEEE_to_ASN {prec emax: Z} (f : binary_float prec emax) : option real :=
   match f with
-  | Bin32 f32 => match f32 with
-              | B754_zero _ _ s => ASN_zero s
-              | B754_infinity _ _ s => ASN_infinity s
-              | B754_nan _ _ _ _ _ => ASN_nan
-              | B754_finite _ _ s m e H =>
-                ASN_finite s radix2 m e (bin32_finite_convertible m e H)
-              end
-  | Bin64 f64 => match f64 with
-              | B754_zero _ _ s => ASN_zero s
-              | B754_infinity _ _ s => ASN_infinity s
-              | B754_nan _ _ _ _ _ => ASN_nan
-              | B754_finite _ _ s m e H =>
-                ASN_finite s radix2 m e (bin64_finite_convertible m e H)
-              end
+  | B754_zero _ _ s => Some (ASN_zero s)
+  | B754_infinity _ _ s => Some (ASN_infinity s)
+  | B754_nan _ _ _ _ _ => Some (ASN_nan)
+  | B754_finite _ _ s m e _ =>
+    match good_real_s m e radix2 with
+    | left G => Some (ASN_finite s radix2 m e G)
+    | right _ => None
+    end
   end.
 
-Definition convertible_ASN : ASN_real -> bool.
-Admitted.
+Fact def_NAN (prec:Z): nan_pl prec 1 = true. Admitted.
 
-Definition ASN_to_IEEE (r : ASN_real) :
-  convertible_ASN r = true -> convertible_IEEE.
-Admitted.
+Definition ASN_to_IEEE (prec emax: Z) (r : real) : option (binary_float prec emax)
+  :=
+    match r with
+    | ASN_zero s => Some (B754_zero prec emax s)
+    | ASN_infinity s => Some (B754_infinity prec emax s)
+    | ASN_nan => Some (B754_nan prec emax true 1 (def_NAN prec))
+    | ASN_finite s b m e x =>
+      match bbounded_s prec emax m e with
+      | left B => Some (B754_finite prec emax s m e B)
+      | right _ => None
+      end
+    end.
 
-Lemma IEEE_to_ASN_reversible (f : convertible_IEEE) :
-  convertible_ASN (IEEE_to_ASN f) = true.
-Admitted.
 
-Lemma round_trip (f : convertible_IEEE) :
-  ASN_to_IEEE (IEEE_to_ASN f) (IEEE_to_ASN_reversible f) = f.
-Admitted.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
