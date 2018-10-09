cc27053689fb7d17ff76f25bd4642905654afdde
Ilia Zaichuk <zoickx@gmail.com>
2018-10-09 20:37:49 +0300

    Clean up FullBin: move abstraction to ASN/Aux

--- coq/Conversion/FullBin.v
+++ coq/Conversion/FullBin.v
@@ -1,38 +1,14 @@
 Require Import ZArith Basics.
-Require Import ASN.ASNDef Conversion.ASN_Bin Conversion.ASN_IEEE Aux.Option Aux.StructTactics.
+Require Import ASN.ASNDef ASN.Aux Conversion.ASN_Bin Conversion.ASN_IEEE Aux.Option Aux.StructTactics.
 Require Import Flocq.IEEE754.Binary Flocq.IEEE754.Bits Flocq.Core.Zaux.
 
 Open Scope Z.
 
-Definition l1 {A B : Type} (f : A -> option B) : (option A -> option B) :=
-  fun oa : option A =>
-    match oa with
-    | Some a => f a
-    | None => None
-    end.
-
-Definition l2 {A B : Type} (f : A -> B) : (option A -> option B) :=
-  fun oa : option A =>
-    match oa with
-    | Some a => Some (f a)
-    | None => None
-    end.
-
-    
-
 Section B32.
 
-  Let prec := 24.
-  Let emax := 128.
-  Let prec_gt_1 : Prec_gt_1 prec.
-  Proof. reflexivity. Qed.
-  
-  Definition b32_to_BER_abstract := IEEE_to_BER_exact prec emax.
 
   Definition b32_to_BER_binary : Z -> option Z :=
     compose (l2 (BER_to_bits false)) (compose b32_to_BER_abstract b32_of_bits).
-
-  Definition BER_to_b32_abstract := BER_to_IEEE_exact prec emax prec_gt_1.
 
   Definition BER_to_b32_binary : Z -> option Z :=
     compose (l2 bits_of_b32) (compose (l1 BER_to_b32_abstract) bits_to_BER).
@@ -42,17 +18,8 @@ End B32.
 
 Section B64.
 
-  Let prec := 53.
-  Let emax := 1024.
-  Let prec_gt_1 : Prec_gt_1 prec.
-  Proof. reflexivity. Qed.
-  
-  Definition b64_to_BER_abstract := IEEE_to_BER_exact prec emax.
-
   Definition b64_to_BER_binary : Z -> option Z :=
     compose (l2 (BER_to_bits false)) (compose b32_to_BER_abstract b32_of_bits).
-
-  Definition BER_to_b64_abstract := BER_to_IEEE_exact prec emax prec_gt_1.
 
   Definition BER_to_b64 : Z -> option Z :=
     compose (l2 bits_of_b32) (compose (l1 BER_to_b32_abstract) bits_to_BER).
