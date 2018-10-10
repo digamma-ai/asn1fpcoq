15bb1231640654ef031b58cda98706ebbd5e5711
Vadim Zaliva <lord@crocodile.org>
2018-10-09 18:22:09 -0700

    renamed high-level functions. re-implemented using monad notation

--- coq/Aux/Option.v
+++ coq/Aux/Option.v
@@ -10,16 +10,3 @@ Definition is_Some_b {A : Type} (x : option A) : bool :=
   | _ => false
   end.
 
-Definition l1 {A B : Type} (f : A -> option B) : (option A -> option B) :=
-  fun oa : option A =>
-    match oa with
-    | Some a => f a
-    | None => None
-    end.
-
-Definition l2 {A B : Type} (f : A -> B) : (option A -> option B) :=
-  fun oa : option A =>
-    match oa with
-    | Some a => Some (f a)
-    | None => None
-    end.
--- coq/Conversion/FullBin.v
+++ coq/Conversion/FullBin.v
@@ -1,36 +1,48 @@
 Require Import ZArith Basics.
-Require Import ASN.ASNDef  Conversion.ASN_Bin Conversion.ASN_IEEE Aux.Option Aux.StructTactics Aux.BinK.
+Require Import ASN.ASNDef  Conversion.ASN_Bin Conversion.ASN_IEEE Aux.StructTactics Aux.BinK.
 Require Import Flocq.IEEE754.Binary Flocq.IEEE754.Bits Flocq.Core.Zaux.
+
+Require Import ExtLib.Structures.Monads.
+Require Import ExtLib.Data.Monads.OptionMonad.
+
+Import MonadNotation.
+Local Open Scope monad_scope.
 
 Open Scope Z.
 
 Section B32.
 
-    Definition b32_to_BER_binary_exact : Z -> option Z :=
-      compose (l2 (BER_to_bits false)) (compose b32_to_BER_abstract b32_of_bits).
+  Definition float32_to_BER_exact (f:Z): option Z :=
+    ab <- b32_to_BER_abstract (b32_of_bits f) ;;
+       ret (BER_to_bits false ab).
 
-    Definition BER_to_b32_binary_exact : Z -> option Z :=
-      compose (l2 bits_of_b32) (compose (l1 BER_to_b32_abstract_exact) bits_to_BER).
+  Definition BER_to_float32_exact (asn:Z): option Z :=
+    bf <- bits_to_BER asn ;;
+       af <- BER_to_b32_abstract_exact bf ;;
+       ret (bits_of_b32 af).
 
-    Definition BER_to_b32_binary_rounded (rounding : mode ) : Z -> option Z :=
-      compose (l2 bits_of_b32)
-        (compose (l1 (BER_to_b32_abstract_rounded rounding))
-          bits_to_BER).
+  Definition BER_to_float32_rounded (rounding: mode) (asn:Z): option Z :=
+    bf <- bits_to_BER asn ;;
+       af <- BER_to_b32_abstract_rounded rounding bf ;;
+       ret (bits_of_b32 af).
 
 End B32.
 
 
 Section B64.
 
-    Definition b64_to_BER_binary_exact : Z -> option Z :=
-      compose (l2 (BER_to_bits false)) (compose b64_to_BER_abstract b64_of_bits).
+    Definition float64_to_BER_exact (f: Z): option Z :=
+    ab <- b64_to_BER_abstract (b64_of_bits f) ;;
+       ret (BER_to_bits false ab).
 
-    Definition BER_to_b64_binary_exact : Z -> option Z :=
-      compose (l2 bits_of_b64) (compose (l1 BER_to_b64_abstract_exact) bits_to_BER).
+    Definition BER_to_float64_exact (asn: Z): option Z :=
+    bf <- bits_to_BER asn ;;
+       af <- BER_to_b64_abstract_exact bf ;;
+       ret (bits_of_b64 af).
 
-    Definition BER_to_b64_binary_rounded (rounding : mode ) : Z -> option Z :=
-      compose (l2 bits_of_b64)
-        (compose (l1 (BER_to_b64_abstract_rounded rounding))
-          bits_to_BER).
+    Definition BER_to_float64rounded (rounding : mode ) (asn:Z): option Z :=
+    bf <- bits_to_BER asn ;;
+       af <- BER_to_b64_abstract_rounded rounding bf ;;
+       ret (bits_of_b64 af).
 
 End B64.
