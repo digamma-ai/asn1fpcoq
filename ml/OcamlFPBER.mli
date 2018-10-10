dbb82d5ee47a68dbcd8c30ac10a421020625e925
Vadim Zaliva <lord@crocodile.org>
2018-10-09 22:24:10 -0700

    OcamlFPBER module

---
+++ ml/OcamlFPBER.mli
@@ -0,0 +1,8 @@
+open FullBin
+open Binary
+
+val ocaml_float64_to_BER_exact: float -> string option
+
+val ocaml_BER_to_float64_exact: string -> float option
+
+val ocaml_BER_to_float64_rounded: mode -> string -> float option
