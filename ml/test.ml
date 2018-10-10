9382e1243f947ea48a59ed7bef512aefacd6255e
Vadim Zaliva <lord@crocodile.org>
2018-10-09 22:28:23 -0700

    test round trip (on -inf)

--- ml/test.ml
+++ ml/test.ml
@@ -1,6 +1,12 @@
 open OcamlFPBER
 
 let () =
-  let _ = ocaml_float64_to_BER_exact Float.neg_infinity in
-  Printf.eprintf "*** Test!\n" ;
+  begin
+    match ocaml_float64_to_BER_exact Float.neg_infinity with
+    | None -> Printf.eprintf "FP->BER Conversion error!\n"
+    | Some a ->
+       match ocaml_BER_to_float64_exact a with
+       | None -> Printf.eprintf "BER->FP Conversion error!\n"
+       | Some f -> Printf.eprintf "Got: %f\n" f
+  end ;
   exit 0
