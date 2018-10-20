81f46b46963fb5fc3b6e6ae14dd5a1ceadf39f20
Vadim Zaliva <lord@crocodile.org>
2018-10-19 18:15:29 -0700

    getting rid of some R deps

--- ml/extracted/Extract.v
+++ ml/extracted/Extract.v
@@ -13,11 +13,18 @@ Set Extraction AccessOpaque.
 
 Extract Inductive Decimal.int => unit [ "(fun _ -> ())" "(fun _ -> ())" ] "(fun _ _ _ -> assert false)".
 
+Extract Inlined Constant Flocq.Core.Defs.F2R => "(fun _ -> assert false)".
+Extract Inlined Constant Flocq.IEEE754.Binary.FF2R => "(fun _ -> assert false)".
+Extract Inlined Constant Flocq.IEEE754.Binary.B2R => "(fun _ -> assert false)".
+Extract Inlined Constant Flocq.IEEE754.Binary.round_mode => "(fun _ -> assert false)".
+Extract Inlined Constant Flocq.Calc.Bracket.inbetween_loc => "(fun _ -> assert false)".
+
 (* NOTE: assumes that this file is compiled from /src *)
 Cd "ml/extracted".
 
 Extraction Library ExtrOcamlBasic.
 Extraction Library ExtrOcamlNatBigInt.
 Extraction Library ExtrOcamlZBigInt.
+
 
 Recursive Extraction Library FullBin.
