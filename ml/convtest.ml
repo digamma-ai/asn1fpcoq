018081179cba7349ee894b5b6bf52d6763d30d4a
Vadim Zaliva <lord@crocodile.org>
2018-10-19 18:47:35 -0700

    more tests, print hex strings

--- ml/convtest.ml
+++ ml/convtest.ml
@@ -1,10 +1,14 @@
 open OcamlFPBER
 
+let pr_hexstring = String.iter (fun c -> Printf.eprintf "%02X " (Char.code c))
+
 let test_roundtrip f =
   match ocaml_float64_to_BER_exact f with
   | None -> Printf.eprintf "FP->BER Conversion error of %f!\n" f ; None
   | Some a ->
-     Printf.eprintf "FP->BER Conversion of %f produced %d BER bytes \n" f (String.length a) ;
+     Printf.eprintf "FP->BER Conversion of %f produced %d BER bytes: [ " f (String.length a) ;
+     pr_hexstring a ;
+     Printf.eprintf "]\n" ;
      let mf = ocaml_BER_to_float64_exact a in
      begin
        match mf with
@@ -16,6 +20,8 @@ let test_roundtrip f =
 let () =
   Printf.eprintf "Testing...\n" ;
   let _ = test_roundtrip 3.1415 in
+  let _ = test_roundtrip (1E2) in
+  let _ = test_roundtrip (-3E9) in
   let _ = test_roundtrip Float.neg_infinity in
   let _ = test_roundtrip Float.nan in
   exit 0
