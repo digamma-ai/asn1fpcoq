75ba71b5c3c0c2403c9d24adb81f627be9a75834
Vadim Zaliva <lord@crocodile.org>
2018-10-24 16:53:22 -0700

    cosmetics

--- ml/convtest.ml
+++ ml/convtest.ml
@@ -23,7 +23,7 @@ let test_no_scl_radix2 f = assert_equal
   (Core.Option.map2 (Some f) (roundtrip r2 false f) float_eqb_nan_t)
   (Some true)
 *)
-let test_no_scl_radix2 f = assert_equal
+let test_no_scl_radix2 f _ = assert_equal
   (match (roundtrip r2 false f) with
   | None -> false
   | Some f' -> float_eqb_nan_t f f')
@@ -31,57 +31,19 @@ let test_no_scl_radix2 f = assert_equal
 
 let normal_numbers_suite =
 "Normal Numbers">:::
-  ["3.1415">::  (fun _ -> test_no_scl_radix2 3.1415);
-   "-3.1415">:: (fun _ -> test_no_scl_radix2 (-3.1415));
-   "3T">::      (fun _ -> test_no_scl_radix2 3E12)]
+  ["3.1415">::  test_no_scl_radix2 3.1415;
+   "-3.1415">:: test_no_scl_radix2 (-3.1415);
+   "3T">::      test_no_scl_radix2 3E12]
 
 let special_values_suite =
 "Special Values">:::
-  ["+Zero">:: (fun _ -> test_no_scl_radix2 0.0);
-  "-Zero">:: (fun _ -> test_no_scl_radix2 (-0.0));
-  "+Inf">:: (fun _ -> test_no_scl_radix2 Float.infinity);
-  "-Inf">:: (fun _ -> test_no_scl_radix2 Float.neg_infinity);
-  "NaN">:: (fun _ -> test_no_scl_radix2 Float.nan)]
+  ["+Zero">:: test_no_scl_radix2 0.0;
+  "-Zero">:: test_no_scl_radix2 (-0.0);
+  "+Inf">:: test_no_scl_radix2 Float.infinity;
+  "-Inf">:: test_no_scl_radix2 Float.neg_infinity;
+  "NaN">:: test_no_scl_radix2 Float.nan]
 
-let _ = run_test_tt_main normal_numbers_suite
-let _ = run_test_tt_main special_values_suite
+let _ =
+  run_test_tt_main normal_numbers_suite ;
+  run_test_tt_main special_values_suite
 
-(*
-
-let test_roundtrip radix scaled f =
-  Printf.eprintf "Testing for %f\n" f ;
-  match ocaml_float64_to_BER_exact radix scaled f with
-  | None -> Printf.eprintf "    ERROR: FP->BER Conversion error of %f!\n" f ; None
-  | Some a ->
-     Printf.eprintf "    FP->BER Conversion of %f produced %d BER bytes: [ " f (String.length a) ;
-     pr_hexstring a ;
-     Printf.eprintf "]\n" ;
-     let mf = ocaml_BER_to_float64_exact a in
-     begin
-       match mf with
-       | None -> Printf.eprintf "    ERROR: FP->BER->FP Conversion back error for %f!\n" f
-       | Some f' ->
-          if float_eqb_nan_t f f' then
-            Printf.eprintf "    PASS: for value %f\n" f
-          else
-            Printf.eprintf "    FAIL: After converting %f got back: %f!\n" f f'
-     end;
-     mf
-
-let () =
-  let s = false in
-  let _ = Printf.eprintf "\n\nTesting finite values\n\n" in
-  let _ = test_roundtrip r s (3.1415) in
-  let _ = test_roundtrip r s (-3.1415) in
-  let _ = test_roundtrip r s (3.0) in
-  let _ = test_roundtrip r s (-3.0) in
-  let _ = test_roundtrip r s (1E2) in
-  let _ = test_roundtrip r s (-3E9) in
-  let _ = Printf.eprintf "\nTesting special values\n\n" in
-  let _ = test_roundtrip r s (0.0) in
-  let _ = test_roundtrip r s (-0.0) in
-  let _ = test_roundtrip r s Float.infinity in
-  let _ = test_roundtrip r s Float.neg_infinity in
-  let _ = test_roundtrip r s Float.nan in
-  exit 0
-*)
