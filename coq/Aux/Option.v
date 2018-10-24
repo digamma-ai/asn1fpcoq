727e84f8032fe1e75d0d504c67341471b397ee3f
Vadim Zaliva <lord@crocodile.org>
2018-10-24 11:11:38 -0700

    using `option_filter`

--- coq/Aux/Option.v
+++ coq/Aux/Option.v
@@ -9,3 +9,6 @@ Definition is_Some_b {A : Type} (x : option A) : bool :=
   | Some _ => true
   | _ => false
   end.
+
+Definition option_filer {T:Type} (P: bool): T -> option T :=
+  fun x => if P then Some x else None.
--- coq/Conversion/FullBin.v
+++ coq/Conversion/FullBin.v
@@ -1,9 +1,10 @@
 Require Import ZArith Basics.
-Require Import Types.ASNDef
-               Conversion.IEEE_ASN.Def
-               Conversion.ASN_Bitstring.Def
-               Conversion.Bitstring_Bits.Def
-               Conversion.BinK.
+Require Import Aux.Option
+        Types.ASNDef
+        Conversion.IEEE_ASN.Def
+        Conversion.ASN_Bitstring.Def
+        Conversion.Bitstring_Bits.Def
+        Conversion.BinK.
 Require Import Flocq.IEEE754.Binary Flocq.IEEE754.Bits Flocq.Core.Zaux.
 
 Require Import ExtLib.Structures.Monads.
@@ -18,12 +19,10 @@ Open Scope Z.
 Notation "f >=> g" := (fun x => pbind (f x) g) (at level 50, left associativity) : monad_scope.
 
 Section B32.
-
   Definition float32_to_BER_exact (target_radix : radix) (scaled : bool) : Z -> option Z :=
-    if (target_radix =? 2)%Z
-    then compose b32_to_BER_abstract b32_of_bits >=> (BER_to_bitstring scaled) >=>
-             compose ret bitstring_to_bits
-    else (fun _ => None).
+    option_filer (target_radix =? 2)%Z >=>
+      compose b32_to_BER_abstract b32_of_bits >=> (BER_to_bitstring scaled) >=>
+      compose ret bitstring_to_bits.
 
   Definition BER_to_float32_exact: Z -> option Z :=
     bits_to_bitstring >=> bitstring_to_BER >=> BER_to_b32_abstract_exact >=>
@@ -37,10 +36,9 @@ End B32.
 Section B64.
 
   Definition float64_to_BER_exact (target_radix : radix) (scaled : bool) : Z -> option Z :=
-    if (target_radix =? 2)%Z
-    then compose b64_to_BER_abstract b64_of_bits >=> (BER_to_bitstring scaled) >=>
-             compose ret bitstring_to_bits
-    else (fun _ => None).
+    option_filer (target_radix =? 2)%Z >=>
+    compose b64_to_BER_abstract b64_of_bits >=> (BER_to_bitstring scaled) >=>
+    compose ret bitstring_to_bits.
 
   Definition BER_to_float64_exact: Z -> option Z :=
     bits_to_bitstring >=> bitstring_to_BER >=> BER_to_b64_abstract_exact >=>
