8243c91e06a831db11a54887c69efafe53db8607
Vadim Zaliva <lord@crocodile.org>
2018-10-24 17:24:54 -0700

    costmeric refactoring

--- ml/convtest.ml
+++ ml/convtest.ml
@@ -3,42 +3,39 @@ open OUnit2
 open Big_int
 open Core
 
-let float_eqb_nan_t f1 f2 =
-  f1 = f2 || (Core.Float.is_nan f1 && Core.Float.is_nan f2)
+let opt_float_eqb_nan_t o1 o2 =
+  match o1, o2 with
+  | None, None -> true
+  | Some f1, Some f2 -> f1 = f2 || (Float.is_nan f1 && Float.is_nan f2)
+  | _, _ -> false
 
 let roundtrip radix scaled f =
   match ocaml_float64_to_BER_exact radix scaled f with
   | None -> None
   | Some a ->
      let mf = ocaml_BER_to_float64_exact a in
-       match mf with
-       | None -> None
-       | Some _ -> mf
+     match mf with
+     | None -> None
+     | Some _ -> mf
 
 let r2 = big_int_of_int 2
 
-(*
-let test_no_scl_radix2 f = assert_equal
-  (Core.Option.map2 (Some f) (roundtrip r2 false f) float_eqb_nan_t)
-  (Some true)
-*)
-let test_no_scl_radix2 f _ = assert_equal
-  (match (roundtrip r2 false f) with
-  | None -> false
-  | Some f' -> float_eqb_nan_t f f')
-  true
+let test_no_scl_radix2 v _ =
+  assert_equal ~cmp:opt_float_eqb_nan_t (roundtrip r2 false v) (Some v)
 
 let normal_numbers_suite =
   "Normal Numbers">:::
-    List.map [3.1415; (-3.1415); 3E12] ~f:(fun v -> string_of_float v >:: test_no_scl_radix2 v)
+    List.map
+      [3.1415; (-3.1415); 3E12]
+      ~f:(fun v -> string_of_float v >:: test_no_scl_radix2 v)
 
 let special_values_suite =
-"Special Values">:::
-  ["+Zero">:: test_no_scl_radix2 0.0;
-  "-Zero">:: test_no_scl_radix2 (-0.0);
-  "+Inf">:: test_no_scl_radix2 Float.infinity;
-  "-Inf">:: test_no_scl_radix2 Float.neg_infinity;
-  "NaN">:: test_no_scl_radix2 Float.nan]
+  "Special Values">:::
+    ["+Zero" >:: test_no_scl_radix2 0.0;
+     "-Zero" >:: test_no_scl_radix2 (-0.0);
+     "+Inf"  >:: test_no_scl_radix2 Float.infinity;
+     "-Inf"  >:: test_no_scl_radix2 Float.neg_infinity;
+     "NaN"   >:: test_no_scl_radix2 Float.nan]
 
 let positive_numbers_suite n =
   "Postivie Numbers">:::
