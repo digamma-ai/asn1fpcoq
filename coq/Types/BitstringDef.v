87865fe518f2571774e5e7fdbd4b71da426d88d2
zoickx <zoickx@gmail.com>
2018-12-07 01:55:26 +0200

    Improve documentation in BitstringDef.v

--- coq/Types/BitstringDef.v
+++ coq/Types/BitstringDef.v
@@ -1,14 +1,27 @@
 Require Import ZArith.
 Require Import ASN1FP.Types.ASNDef
                ASN1FP.Aux.Roundtrip ASN1FP.Aux.Bits ASN1FP.Aux.StructTactics ASN1FP.Aux.Tactics.
+
 Require Import Template.All Switch.Switch Strings.String Lists.List.
+
 Import ListNotations.
 
 Open Scope Z.
 
+(* TODO: consult the doc *)
 Definition real_id_b := 9.
+
+(*
+ * only binary encoding is supported
+ * [ 8.5.6 (a) ]
+ *   NOTE: "SpecialRealValues" ([ 8.5.9 ]) are defined separately
+ *)
 Definition binary_bit := 1.
 
+(*
+ * "SpecialRealValues" as bit strings including all parts of the encoding
+ * [ 8.5.9 ]
+ *)
 Definition pzero_b   := 2304.
 Definition nzero_b   := 590147.
 Definition pinf_b    := 590144.
@@ -16,14 +29,14 @@ Definition ninf_b    := 590145.
 Definition nan_b     := 590146.
 
 Run TemplateProgram
-    (mkSwitch Z Z.eqb
-              [(pzero_b,    "pzero") ;
-                  (nzero_b,     "nzero") ;
-                  (pinf_b,       "pinf") ;
-                  (ninf_b,       "ninf") ;
-                  (nan_b,         "nan")]
-              "BER_specials" "classify_BER"
-    ).
+  (mkSwitch Z Z.eqb
+    [ (pzero_b, "pzero") ;
+      (nzero_b, "nzero") ;
+      (pinf_b,   "pinf") ;
+      (ninf_b,   "ninf") ;
+      (nan_b,     "nan") ]
+    "BER_specials" "classify_BER"
+  ).
 
 Definition valid_special (val : Z) : bool :=
   match (classify_BER val) with
@@ -31,11 +44,28 @@ Definition valid_special (val : Z) : bool :=
   | None   => false
   end.
 
+(*
+ * can [co] be classified as a
+ * content length octet in short form
+ *)
 Definition correct_short_co (co e_olen m_olen : Z) : bool :=
   ((e_olen + m_olen) <? co) && (co <? 128).
 
+(*
+ * do the atomic parts constitute a valid BER-encoded real
+ * in short form:
+ *   id - identifier octet(s) [ 8.1.2 ]
+ *   co - length octet(s) [ 8.1.3 ]
+ *   t  - encoding type bit [ 8.5.6 ]
+ *   s  - sign bit [ 8.5.7.1 ]
+ *   bb - base bits [ 8.5.7.2 ]
+ *   ff - scaling factor bits [ 8.5.7.3 ]
+ *   ee - exponent format bits [ 8.5.7.4 ]
+ *   e  - exponent bits [ 8.5.7.4 ]
+ *   m  - mantissa bits [ 8.5.7.5 ]
+ *)
 Definition valid_short (id co t s bb ff ee e m : Z) : bool :=
-     (Z.eqb id real_id_b)                  (* identifier is "REAL" *)
+     (Z.eqb id real_id_b)                  (* identifier is "REAL" *) (* TODO *)
   && (correct_short_co co (ee+1) (olen m)) (* encoding length is correct *)
   && (Z.eqb t 1)                           (* encoding is binary *)
   && (Z.ltb (-1)  s) && (Z.ltb  s 2)       (* sign bit is well-formed *)
@@ -43,12 +73,27 @@ Definition valid_short (id co t s bb ff ee e m : Z) : bool :=
   && (Z.ltb (-1) ff) && (Z.ltb ff 4)       (* scaling factor is well-formed *)
   && (Z.ltb (-1) ee) && (Z.ltb ee 3)       (* exponent length is well-formed *)
   && (Z.ltb (olen e) (ee + 2))             (* exponent length is correct *)
-  && (Z.ltb (-1) e)                        (* exponent is non-negative (it is two's complement) *)
+  && (Z.ltb (-1) e)                        (* exponent is non-negative *)
+                                           (* (it is two's complement) *)
   && (Z.ltb 0 m).                          (* mantissa is positive *)
 
 Definition correct_long_co (co e_olen m_olen : Z) : bool :=
   ((e_olen + m_olen + 1) <? co) && (co <? 128).
 
+(*
+ * do the atomic parts constitute a valid BER-encoded real
+ * in long form:
+ *   id - identifier octet(s) [ 8.1.2 ]
+ *   co - length octet(s) [ 8.1.3 ]
+ *   t  - encoding type bit [ 8.5.6 ]
+ *   s  - sign bit [ 8.5.7.1 ]
+ *   bb - base bits [ 8.5.7.2 ]
+ *   ff - scaling factor bits [ 8.5.7.3 ]
+ *   ee - exponent format bits [ 8.5.7.4 ]
+ *   eo - exponent length octet [ 8.5.7.4 (d) ]
+ *   e  - exponent bits [ 8.5.7.4 ]
+ *   m  - mantissa bits [ 8.5.7.5 ]
+ *)
 Definition valid_long (id co t s bb ff ee eo e m : Z) : bool :=
      (Z.eqb id real_id_b)              (* identifier is "REAL" *)
   && (correct_long_co co eo (olen m))  (* encoding length is correct *)
@@ -59,7 +104,8 @@ Definition valid_long (id co t s bb ff ee eo e m : Z) : bool :=
   && (Z.eqb ee 3)                      (* exponent is in long form *)
   && (Z.ltb (-1) eo) && (Z.ltb eo 256) (* exponent length is well-formed *)
   && (Z.ltb (olen e) (eo + 1))         (* exponent length is correct *)
-  && (Z.ltb (-1) e)                    (* exponent is non-negative (it is two's complement) *)
+  && (Z.ltb (-1) e)                    (* exponent is non-negative *)
+                                       (* (it is two's complement) *)
   && (Z.ltb 0 m).                      (* mantissa is positive *)
 
 Inductive BER_bitstring :=
@@ -69,7 +115,7 @@ Inductive BER_bitstring :=
   | long  (id co t s bb ff ee eo e m : Z) :
       (valid_long id co t s bb ff ee eo e m) = true -> BER_bitstring.
 
-(* TODO *)
+(* TODO: simplify? *)
 Definition BER_bitstring_eqb (b1 b2 : BER_bitstring) : bool :=
   match b1, b2 with
   | special val1, special val2 => Z.eqb val1 val2
