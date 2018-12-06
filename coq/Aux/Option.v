669af8aaa8b1848d158219c0296b97211552a2a0
zoickx <zoickx@gmail.com>
2018-12-07 00:48:18 +0200

    Clean up Option.v

--- coq/Aux/Option.v
+++ coq/Aux/Option.v
@@ -10,5 +10,5 @@ Definition is_Some_b {A : Type} (x : option A) : bool :=
   | _ => false
   end.
 
-Definition option_filer {T:Type} (P: bool): T -> option T :=
+Definition option_filer {T : Type} (P : bool) : T -> option T :=
   fun x => if P then Some x else None.
