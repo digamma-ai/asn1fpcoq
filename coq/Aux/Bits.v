413731a0a561fb9618b5e4f2961eb16e8b2c6d2a
zoickx <zoickx@gmail.com>
2018-12-07 00:40:43 +0200

    Refactor Bits.v, add missing documentation

--- coq/Aux/Bits.v
+++ coq/Aux/Bits.v
@@ -35,6 +35,7 @@ Section Length.
    *  
    *  number of bits divided by 8
    *  rounded toward positive infinity
+   *  (workaround for division with rounding toward +inf)
    *)
   Definition blen_to_olen (n : Z) : Z :=
     (n + 7) / 8.
@@ -42,7 +43,6 @@ Section Length.
   (*
    *  smallest number of octets enough
    *  to encode a given number in binary
-   *  (workaround for division with rounding toward +inf)
    *)
   Definition olen (n : Z) : Z :=
     blen_to_olen (blen n).
@@ -51,6 +51,10 @@ End Length.
 
 Section Twos_comp_def.
 
+  (*
+   * all definitions assume
+   * b bits, number n
+   *)
   Variable b n : Z.
   Let r := 2^(b-1).
   Let dr := 2*r.
@@ -98,6 +102,10 @@ Section Twos_comp_def.
   Definition twos_olen : Z :=
     blen_to_olen (twos_blen).
 
+  (*
+   * two's complement of n on b bits if possible
+   * n if impossible
+   *)
   Definition twos_comp_extended : Z :=
   if (b <? 0)
   then n
@@ -111,120 +119,91 @@ Section Twos_comp_def.
                       then n - dr
                       else n.
 
-  Definition twos_l :=
-    negb (b <? 0) && (- r - 1 <? n) && (n <? 0).
-
-  Definition twos_m :=
-    negb (b <? 0) && (-1 <? n) && (n <? r).
-
-  Definition twos_r :=
-    negb (b <? 0) && (r - 1 <? n) && (n <? dr).
-
   Definition twos_dom :=
     negb (b <? 0) && (-r - 1 <? n) && (n <? r).
-
   Definition twos_ran :=
     negb (b <? 0) && (-1 <? n) && (n <? dr).
 
+  (* some useful subsets of twos_comp_extended's domain: *)
+  (* "left" *)
+  Definition twos_l :=
+    negb (b <? 0) && (- r - 1 <? n) && (n <? 0).
+  (* "middle" *)
+  Definition twos_m :=
+    negb (b <? 0) && (-1 <? n) && (n <? r).
+  (* "right" *)
+  Definition twos_r :=
+    negb (b <? 0) && (r - 1 <? n) && (n <? dr).
+
 End Twos_comp_def.
 
-Section Twos_comp_prop.
+Section Twos_comp_proof.
 
-    Lemma twos_comp_l_ran (b n : Z) :
-      twos_l b n = true ->
-      twos_r b (twos_comp_extended b n) = true.
-    Proof.
-      unfold twos_l, twos_r, twos_comp_extended.
-      remember (2^(b-1)) as r.
-      assert (R: 0 <= r). { subst. apply (Z.pow_nonneg 2). lia. }
-      intros H; repeat split_andb.
-      repeat break_match; split_andb_goal;
-        try rewrite Z.ltb_ge in *; rewrite Z.ltb_lt in *;
-          try lia;
-            inversion H; inversion H1.
-    Qed.
+  (* from "left", twos_comp_extended always brings to "right" *)
+  Lemma twos_comp_l_ran (b n : Z) :
+    twos_l b n = true ->
+    twos_r b (twos_comp_extended b n) = true.
+  Proof.
+    unfold twos_l, twos_r, twos_comp_extended.
+    remember (2^(b-1)) as r.
+    assert (R: 0 <= r). { subst. apply (Z.pow_nonneg 2). lia. }
+    intros H; repeat split_andb.
+    repeat break_match; split_andb_goal;
+      try rewrite Z.ltb_ge in *; rewrite Z.ltb_lt in *;
+        try lia;
+          inversion H; inversion H1.
+  Qed.
+  
+  (* from "middle", twos_comp_extended always brings to "middle" *)
+  Lemma twos_comp_m_ran (b : Z) (n : Z) :
+    twos_m b n = true ->
+    twos_m b (twos_comp_extended b n) = true.
+  Proof.
+    unfold twos_m, twos_m, twos_comp_extended.
+    remember (2^(b-1)) as r.
+    assert (R: 0 <= r). { subst. apply (Z.pow_nonneg 2). lia. }
+    intros H; repeat split_andb.
+    repeat break_match; split_andb_goal;
+      try rewrite Z.ltb_ge in *; rewrite Z.ltb_lt in *;
+        try lia;
+          inversion H; inversion H1.
+  Qed.
+  
+  (* from "right", twos_comp_extended always brings to "left" *)
+  Lemma twos_comp_r_ran (b : Z) (n : Z) :
+    twos_r b n = true ->
+    twos_l b (twos_comp_extended b n) = true.
+  Proof.
+    unfold twos_r, twos_l, twos_comp_extended.
+    remember (2^(b-1)) as r.
+    assert (R: 0 <= r). { subst. apply (Z.pow_nonneg 2). lia. }
+    intros H; repeat split_andb.
+    repeat break_match; split_andb_goal;
+      try rewrite Z.ltb_ge in *; rewrite Z.ltb_lt in *;
+        try lia;
+          inversion H; inversion H1.
+  Qed.
+  
+  Lemma twos_comp_extended_roundtrip (b : Z) (n : Z) :
+    bool_het_inverse
+      Z Z Z
+      (twos_comp_extended b)
+      (twos_comp_extended b)
+      Z.eqb
+      n.
+  Proof.
+    unfold bool_het_inverse, twos_comp_extended.
+    remember (2^(b-1)) as r.
+    repeat break_match;
+      try rewrite Z.ltb_lt in *;
+      try rewrite Z.ltb_nlt in *;
+      rewrite Z.eqb_eq in *;
+      lia.
+  Qed.
 
-    Lemma twos_comp_m_ran (b : Z) (n : Z) :
-      twos_m b n = true ->
-      twos_m b (twos_comp_extended b n) = true.
-    Proof.
-      unfold twos_m, twos_m, twos_comp_extended.
-      remember (2^(b-1)) as r.
-      assert (R: 0 <= r). { subst. apply (Z.pow_nonneg 2). lia. }
-      intros H; repeat split_andb.
-      repeat break_match; split_andb_goal;
-        try rewrite Z.ltb_ge in *; rewrite Z.ltb_lt in *;
-          try lia;
-            inversion H; inversion H1.
-    Qed.
+End Twos_comp_proof.
 
-    Lemma twos_comp_r_ran (b : Z) (n : Z) :
-      twos_r b n = true ->
-      twos_l b (twos_comp_extended b n) = true.
-    Proof.
-      unfold twos_r, twos_l, twos_comp_extended.
-      remember (2^(b-1)) as r.
-      assert (R: 0 <= r). { subst. apply (Z.pow_nonneg 2). lia. }
-      intros H; repeat split_andb.
-      repeat break_match; split_andb_goal;
-        try rewrite Z.ltb_ge in *; rewrite Z.ltb_lt in *;
-          try lia;
-            inversion H; inversion H1.
-    Qed.
-
-    Lemma twos_blen_untwos (b : Z) (n : Z) :
-      (n >? 0) &&
-      twos_ran b n = true ->
-      twos_blen (twos_comp_extended b n) = blen n + 1.
-    Proof.
-      unfold twos_ran, twos_comp_extended, twos_blen, blen, log2_pinf.
-      remember (2^(b-1)) as r.
-      assert (R: 0 <= r). { subst. apply (Z.pow_nonneg 2). lia. }
-      intros H; repeat split_andb.
-
-      repeat break_match;
-        try rewrite Z.ltb_ge in *; rewrite Z.ltb_lt in *;
-          try lia.
-
-      - subst n. inversion H0.
-      - subst n. inversion H0.
-      - inversion H2.
-      - inversion H1.
-      - subst n. remember (Z.pos p) as n. lia.
-      - subst n. remember (Z.pos p) as n. lia.
-      - admit.
-      - admit.
-    Abort.
-
-
-    Lemma twos_olen_untwos (o : Z) (n : Z) :
-      let b := 8*o in
-      twos_ran b n = true ->
-      twos_olen (twos_comp_extended b n) <= olen n.
-    Proof.
-    Admitted.
-
-Lemma twos_comp_extended_roundtrip (b : Z) (n : Z) :
-  bool_het_inverse
-    Z Z Z
-    (twos_comp_extended b)
-    (twos_comp_extended b)
-    Z.eqb
-    n.
-Proof.
-  unfold bool_het_inverse, twos_comp_extended.
-  remember (2^(b-1)) as r.
-  repeat break_match;
-    try rewrite Z.ltb_lt in *;
-    try rewrite Z.ltb_nlt in *;
-    rewrite Z.eqb_eq in *;
-    lia.
-Qed.
-
-End Twos_comp_prop.
-
-
-Section Operations.
+Section Split_concat.
 
   (*
    *  given two numbers [fst] and [snd] representing two bit strings,
@@ -233,11 +212,16 @@ Section Operations.
   Definition join_bits_ext (bits_snd : Z) (fst snd : Z) : Z :=
     (Z.shiftl fst bits_snd + snd)%Z.
 
+  (*  concatenate [fst] and [snd], with no leading zeroes in [snd] *)
   Definition join_bits (fst snd : Z) : Z :=
     join_bits_ext (blen snd) fst snd.
 
-  Definition join_octets_ext (snd_olen : Z) (fst snd : Z) : Z :=
-    join_bits_ext (8 * snd_olen) fst snd.
+  (*
+   *  given two numbers [fst] and [snd] representing two bit strings,
+   *  concatentate them, using [octets_snd] octets to represent [snd] 
+   *)
+  Definition join_octets_ext (octets_snd : Z) (fst snd : Z) : Z :=
+    join_bits_ext (8 * octets_snd) fst snd.
 
   (*
    *  concatenate two numbers, encoding the [snd] in exactly
@@ -272,7 +256,7 @@ Section Operations.
    *  with the left part having length of [octets_snd] octets
    *
    *  NOTE: 
-   *    if overall number of bits is not divisible by 8, leading zeros are assumed:
+   *    if overall number of bits is not divisible by 8, leading zeros assumed:
    *    the right part of the split always has a number of bits divisible by 8
    *    For example:
    *          110011001111 -> 00001100  11001111,
@@ -281,4 +265,4 @@ Section Operations.
   Definition split_octets_by_fst (octets_fst : Z) (b : Z) : Z * Z :=
     split_octets_by_snd (olen b - octets_fst) b.
 
-End Operations.
+End Split_concat.
