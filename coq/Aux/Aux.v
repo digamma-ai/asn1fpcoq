af07398ed47a3502471836fe677dacc433410061
zoickx <zoickx@gmail.com>
2018-12-06 18:20:56 +0200

    Clean up Aux/Aux.v

--- coq/Aux/Aux.v
+++ coq/Aux/Aux.v
@@ -1,4 +1,5 @@
-Definition curry {A B C: Type} (f: (A*B)->C) (x:A) (y:B) := f (x, y).
+Definition curry {A B C : Type} (f : (A * B) -> C) (x : A) (y : B) :=
+  f (x, y).
 
-Definition uncurry {A B C: Type} (g:A->B->C) (t:A*B) :=
-  let '(x,y) := t in g x y.
+Definition uncurry {A B C : Type} (g : A -> B -> C) (t : A * B) :=
+  let '(x, y) := t in g x y.
