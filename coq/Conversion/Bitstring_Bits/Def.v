ba24e7459ddbe64c7ac526dcf9ef42da766286e4
zoickx <zoickx@gmail.com>
2019-01-09 02:16:47 +0200

    Rederfine `bits_of_bitsting`

--- coq/Conversion/Bitstring_Bits/Def.v
+++ coq/Conversion/Bitstring_Bits/Def.v
@@ -2,6 +2,114 @@ Require Import ZArith.
 Require Import ASN1FP.Types.BitstringDef
                ASN1FP.Aux.Bits.
 
+Definition append_e (e m : Z) :=
+  join_octets e m.
+
+Section Short.
+
+  Variable id co t s bb ff ee e m : Z.
+  Hypothesis V : valid_short id co t s bb ff ee e m = true.
+  Let b := short id co t s bb ff ee e m V.
+
+  (* construct information octet *)
+  Let ffee := join_bits_ext 2 ff ee.
+  Let bbffee := join_bits_ext 4 bb ffee.
+  Let sbbffee := join_bits_ext 6 s bbffee.
+  Let i := join_bits_ext 7 t sbbffee.
+
+  (* join main parts by octets *)
+  Let m_olen := co - ee - 2.
+  Let e_m := join_octets_ext m_olen e m.
+
+  Lemma m_olen_correct_s :
+    olen m <= m_olen.
+  Admitted.
+
+  Let e_m_olen := co - 1.
+  Let i_e_m := join_octets_ext e_m_olen i e_m.
+
+  Lemma e_m_olen_correct_s :
+    olen e_m <= e_m_olen.
+  Admitted.
+
+  Let co_i_e_m := join_octets_ext co co i_e_m.
+
+  Lemma i_e_m_olen_correct_s :
+    olen i_e_m <= co.
+  Admitted.
+
+  Let id_co_i_e_m := join_octets_ext (co + 1) id co_i_e_m.
+
+  Lemma co_i_e_m_olen_correct_s :
+    olen co_i_e_m <= co + 1.
+  Admitted.
+
+  Definition bits_of_short_bitstring_aux :=
+    id_co_i_e_m.
+
+End Short.
+
+Section Long.
+
+  Variable id co t s bb ff ee eo e m : Z.
+  Hypothesis V : valid_long id co t s bb ff ee eo e m = true.
+  Let b := long id co t s bb ff ee eo e m V.
+
+  (* construct information octet *)
+  Let ffee := join_bits_ext 2 ff ee.
+  Let bbffee := join_bits_ext 4 bb ffee.
+  Let sbbffee := join_bits_ext 6 s bbffee.
+  Let i := join_bits_ext 7 t sbbffee.
+
+  (* join main parts by octets *)
+  Let m_olen := co - eo - 2.
+  Let e_m := join_octets_ext m_olen e m.
+
+  Lemma m_olen_correct_l :
+    olen m <= m_olen.
+  Admitted.
+
+  Let e_m_olen := co - 2.
+  Let eo_e_m := join_octets_ext e_m_olen i e_m.
+
+  Lemma e_m_olen_correct_l :
+    olen e_m <= e_m_olen.
+  Admitted.
+
+  Let eo_e_m_olen := co - 1.
+  Let i_eo_e_m := join_octets_ext eo_e_m_olen i eo_e_m.
+
+  Lemma eo_e_m_olen_correct :
+    olen eo_e_m <= eo_e_m_olen.
+  Admitted.
+
+  Let co_i_eo_e_m := join_octets_ext co co i_eo_e_m.
+
+  Lemma i_e_m_olen_correct_l :
+    olen i_eo_e_m <= co.
+  Admitted.
+
+  Let id_co_i_eo_e_m := join_octets_ext (co + 1) id co_i_eo_e_m.
+
+  Lemma co_i_e_m_olen_correct_l :
+    olen co_i_eo_e_m <= co + 1.
+  Admitted.
+
+  Definition bits_of_long_bitstring_aux :=
+    id_co_i_eo_e_m.
+
+End Long.
+
+Definition bits_of_bitstring (b : BER_bitstring) :=
+  match b with
+  | special val => val
+  | short id co t s bb ff ee e m x =>
+      bits_of_short_bitstring_aux id co t s bb ff ee e m
+  | long id co t s bb ff ee eo e m x =>
+      bits_of_long_bitstring_aux id co t s bb ff ee eo e m
+  end.
+
+(*
 Definition bits_of_bitstring (b : BER_bitstring) : Z :=
   match b with
   | special val =>
@@ -46,6 +154,7 @@ Definition bits_of_bitstring (b : BER_bitstring) : Z :=
                     (join_octets_ext (em_olen - exp_olen_o) exponent
                       significand))))))))
   end.
+*)
 
 Definition bitstring_of_bits (b : Z) : option BER_bitstring :=
   match classify_BER b with
