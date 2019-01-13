0aa1bf40453346c6076dc931de727dab587406ef
Vadim Zaliva <lord@crocodile.org>
2019-01-12 20:43:13 -0800

    refactored `join_cont` into definition and 2 lemmas

--- coq/Aux/BitContainer.v
+++ coq/Aux/BitContainer.v
@@ -8,48 +8,61 @@ Definition nblen (n : Z) : nat := Z.to_nat (Z.log2 n + 1).
 
 Inductive container (l : nat) :=
   cont (v : Z) (N : 0 <= v) (L : (nblen v <= l)%nat) : container l.
-  
-Definition join_cont {l1 l2 : nat} (c1 : container l1) (c2 : container l2)
-  : container (l1 + l2).
+
+Lemma join_nonneg (l2 : nat) {v1 v2 : Z} (N1: 0 <= v1) (N2: 0 <= v2):
+  0 <= v1 * two_power_nat l2 + v2.
 Proof.
-  destruct c1 as [v1 N1 L1].
-  destruct c2 as [v2 N2 L2].
-  remember (l1 + l2)%nat as l.
-  remember (v1 * (two_power_nat l2) + v2) as v.
-  assert (N : 0 <= v).
+  rewrite two_power_nat_correct.
+  rewrite Zpower_nat_Z.
+  remember (Z.of_nat l2) as p.
+  remember (2 ^ p) as p2.
+  assert(0 <= p2).
   {
     subst.
-    rewrite two_power_nat_correct.
-    rewrite Zpower_nat_Z.
-    remember (Z.of_nat l2) as p.
-    remember (2 ^ p) as p2.
-    assert(0 <= p2).
-    {
-      subst.
-      apply Z.pow_nonneg.
-      lia.
-    }
-    apply Z.add_nonneg_nonneg; auto.
-    apply Z.mul_nonneg_nonneg; auto.
+    apply Z.pow_nonneg.
+    lia.
   }
-  assert (L : (nblen v <= l)%nat).
-  {
-    unfold nblen in *.
-    apply Nat2Z.inj_le.
-    rewrite Z2Nat.id.
-    -
-      subst.
-      rewrite two_power_nat_correct in *.
-      rewrite Zpower_nat_Z in *.
+  apply Z.add_nonneg_nonneg; auto.
+  apply Z.mul_nonneg_nonneg; auto.
+Qed.
+
+Lemma join_nblen
+      {l1 l2 : nat}
+      {v1 v2: Z}
+      (N1: 0 <= v1) (N2: 0 <= v2)
+      (L1 : (nblen v1 <= l1)%nat)
+      (L2 : (nblen v2 <= l2)%nat):
+  (nblen (v1 * two_power_nat l2 + v2) <= l1 + l2)%nat.
+Proof.
+  unfold nblen in *.
+  apply Nat2Z.inj_le.
+  rewrite Z2Nat.id.
+  -
+    apply Nat2Z.inj_le in L1.
+    apply Nat2Z.inj_le in L2.
+    rewrite Z2Nat.id in L1.
+    rewrite Z2Nat.id in L2.
+    +
+      rewrite Z.add_1_r in *.
       admit.
-    -
-      remember (Z.log2 v) as x.
-      assert(0<=x). subst;apply Z.log2_nonneg.
-      lia.
-  }
-  exact (cont l v N L).
+    +
+      assert(0<=Z.log2 v2) by apply Z.log2_nonneg; lia.
+    +
+      assert(0<=Z.log2 v1) by apply Z.log2_nonneg; lia.
+  -
+    assert(0<=(Z.log2 (v1 * two_power_nat l2 + v2))) by apply Z.log2_nonneg.
+    lia.
 Admitted.
 
+Definition join_cont {l1 l2 : nat} (c1 : container l1) (c2 : container l2)
+  : container (l1 + l2) :=
+  match c1, c2 with
+  | cont _ v1 N1 L1, cont _ v2 N2 L2 =>
+    cont (l1 + l2)
+         (v1 * two_power_nat l2 + v2)
+         (join_nonneg l2 N1 N2)
+         (join_nblen N1 N2 L1 L2)
+  end.
 
 Definition split_cont {l1 l2: nat} (c : container (l1+l2))
   : container l1 * container l2.
