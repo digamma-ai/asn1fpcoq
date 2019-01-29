c3eb7ead218c6c0958b6caf2a1bd9cc71c14d1f9
Ilia Zaichuk <zoickx@gmail.com>
2019-01-29 02:18:30 +0200

    Work on splitting

--- coq/Conversion/Bitstring_Bits/Def.v
+++ coq/Conversion/Bitstring_Bits/Def.v
@@ -5,64 +5,67 @@ Require Import Lia.
 
 Open Scope Z.
 
-(* TODO: restructure *)
+Section CommonLengths.
 
-Definition cont1 := container 1.
-Definition cont2 := container 2.
-Definition cont8 := container 8.
+  Definition cont1 := container 1.
+  Definition cont2 := container 2.
+  Definition cont8 := container 8.
 
-(* create containers for commong lengths *)
-Definition b1_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 1)%nat) : cont1
+  (* create containers for commong lengths *)
+  Definition b1_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 1)%nat) : cont1
   := cont 1 v N L.
-Definition b2_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 2)%nat) : cont2
+  Definition b2_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 2)%nat) : cont2
   := cont 2 v N L.
-Definition b8_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 8)%nat) : cont8
+  Definition b8_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 8)%nat) : cont8
   := cont 8 v N L.
 
-(* create and append containers of common lengths *)
-Definition append_b1_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 1)%nat)
-           {l : nat} (c : container l)
+  (* create and append containers of common lengths *)
+  Definition append_b1_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 1)%nat)
+          {l : nat} (c : container l)
   : container (1 + l) := join_cont (b1_cont v N L) c.
 
-Definition append_b2_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 2)%nat)
-           {l : nat} (c : container l)
+  Definition append_b2_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 2)%nat)
+          {l : nat} (c : container l)
   : container (2 + l) := join_cont (b2_cont v N L) c.
 
-Definition append_b8_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 8)%nat)
-           {l : nat} (c : container l)
+  Definition append_b8_cont (v : Z) (N : 0 <= v) (L : (nblen v <= 8)%nat)
+          {l : nat} (c : container l)
   : container (8 + l) := join_cont (b8_cont v N L) c.
-  
-(* cut containers of common lengths (from left *)
-Definition cut_b1_cont {l : nat} (c : container (1 + l))
+
+  (* cut containers of common lengths (from left *)
+  Definition cut_b1_cont {l : nat} (c : container (1 + l))
   : cont1 * container l := split_cont c.
 
-Definition cut_b2_cont {l : nat} (c : container (2 + l))
+  Definition cut_b2_cont {l : nat} (c : container (2 + l))
   : cont2 * container l := split_cont c.
 
-Definition cut_b8_cont {l : nat} (c : container (8 + l))
+  Definition cut_b8_cont {l : nat} (c : container (8 + l))
   : cont8 * container l := split_cont c.
 
-(* these might or might not be useful *)
-Definition cut_append_b1 (v : Z) (N : 0 <= v) (L : (nblen v <= 1)%nat)
-           {l : nat} (c : container l) :
+  (* these might or might not be useful *)
+  Definition cut_append_b1 (v : Z) (N : 0 <= v) (L : (nblen v <= 1)%nat)
+          {l : nat} (c : container l) :
   cut_b1_cont (append_b1_cont v N L c) = (b1_cont v N L, c).
-Proof. apply split_join_roundtrip. Qed.
+  Proof. apply split_join_roundtrip. Qed.
 
-Definition cut_append_b2 (v : Z) (N : 0 <= v) (L : (nblen v <= 2)%nat)
-           {l : nat} (c : container l) :
+  Definition cut_append_b2 (v : Z) (N : 0 <= v) (L : (nblen v <= 2)%nat)
+          {l : nat} (c : container l) :
   cut_b2_cont (append_b2_cont v N L c) = (b2_cont v N L, c).
-Proof. apply split_join_roundtrip. Qed.
+  Proof. apply split_join_roundtrip. Qed.
 
-Definition cut_append_b8 (v : Z) (N : 0 <= v) (L : (nblen v <= 8)%nat)
-           {l : nat} (c : container l) :
+  Definition cut_append_b8 (v : Z) (N : 0 <= v) (L : (nblen v <= 8)%nat)
+          {l : nat} (c : container l) :
   cut_b8_cont (append_b8_cont v N L c) = (b8_cont v N L, c).
-Proof. apply split_join_roundtrip. Qed.
+  Proof. apply split_join_roundtrip. Qed.
 
-Section outer_sec.
+End CommonLengths.
 
-  Let z2n := Z.to_nat.
-  Let c2z {l : nat} (c : container l) := Z_of_cont c.
-  Let c2n {l : nat} (c : container l) := z2n (c2z c).
+
+Section NormalBitStrings.
+
+  Definition z2n := Z.to_nat.
+  Definition c2z {l : nat} (c : container l) := Z_of_cont c.
+  Definition c2n {l : nat} (c : container l) := z2n (c2z c).
 
   Inductive BER_nbs :=
   | short_nbs
@@ -80,6 +83,31 @@ Section outer_sec.
   Inductive BER_bs_aux :=
   | special_aux (val : Z) : BER_bs_aux
   | normal_aux (b : BER_nbs) : BER_bs_aux.
+
+  Definition valid_short_nbs {l1 l2 l3 l4 l5 l6 l7 l8 l9 : nat}
+             (id : container l1)
+             (co : container l2)
+             (t  : container l3)
+             (s  : container l4)
+             (bb : container l5)
+             (ff : container l6)
+             (ee : container l7)
+             (e  : container l8)
+             (m  : container l9) : bool.
+  Admitted.
+
+  Definition valid_long_nbs {l1 l2 l3 l4 l5 l6 l7 l8 l9 l10 : nat}
+             (id : container l1)
+             (co : container l2)
+             (t  : container l3)
+             (s  : container l4)
+             (bb : container l5)
+             (ff : container l6)
+             (ee : container l7)
+             (eo : container l8)
+             (e  : container l9)
+             (m  : container l10) : bool.
+  Admitted.
 
   Definition bitstring_of_bsaux (b : BER_bs_aux) : BER_bitstring.
   Admitted.
@@ -294,30 +322,66 @@ Section outer_sec.
     (nblen m <= z2n (8*(co - eo - 2)))%nat.
   Admitted.
 
-Section normal.
+End NormalBitStrings.
 
-  Let info_nblen := 24%nat.
-    
-  (* joining *)
 
-  (*
-  Definition content_nblen (b : BER_nbs) : nat :=
-    match b with
-    | short_nbs _ co _ _ _ _ ee _ _ _ 
-      => (z2n (8 * (ee + 1)) + z2n (8 * (co - ee - 2)))
-    | long_nbs _ co _ _ _ _ _ eo _ _ _
-      => 8 + (z2n (8 * eo) + z2n (8 * (co - eo - 2)))
-    end.
+Section Lengths.
+
+  Definition info_nblen := 24%nat.
   
   Definition nbs_nblen (b : BER_nbs) : nat :=
     match b with
-    | short_nbs _ co _ _ _ _ ee _ _ _ 
-      => info_nblen + content_nblen b
-    | long_nbs _ co _ _ _ _ _ eo _ _ _
-      => info_nblen + content_nblen b
+    | short_nbs id co t s bb ff ee e m _ _ _ _ => 8 * (c2n co + 2)
+    | long_nbs id co t s bb ff ee eo e m _ _ _ _ => 8 * (c2n co + 2)
     end.
-*)
+
+  Definition content_nblen (b : BER_nbs) : nat :=
+    match b with
+    | short_nbs id co t s bb ff ee e m _ _ _ _ => 8 * (c2n co - 1)
+    | long_nbs id co t s bb ff ee eo e m _ _ _ _ => 8 * (c2n co - 1)
+    end.
+
+  Lemma short_nblen_correct {l1 l2 : nat} (co : container l1) (ee : container l2) :
+      (8 * (c2n ee + 1) + 8 * (c2n co - c2n ee - 2))%nat = (8 * (c2n co - 1))%nat.
+  Proof.
+    remember (c2n co) as x; remember (c2n ee) as y.
+    rewrite <- Nat.mul_add_distr_l.
+    replace (y + 1 + (x - y - 2))%nat with (x - 1)%nat.
+    reflexivity.
+  Admitted.
+
+  Lemma long_nblen_correct {l1 l2 : nat} (co : container l1) (eo : container l2) :
+      (8 + (8 * c2n eo + 8 * (c2n co - c2n eo - 2)))%nat = (8 * (c2n co - 1))%nat.
+  Proof.
+    remember (c2n co) as x; remember (c2n eo) as y.
+    rewrite <- Nat.mul_add_distr_l.
+  Admitted.
+
+  Lemma nbs_nblen_correct (b : BER_nbs) :
+    (info_nblen + content_nblen b)%nat = nbs_nblen b.
+  Proof.
+    unfold info_nblen.
+    destruct b.
+    - (* short *)
+      unfold content_nblen, nbs_nblen.
+      remember (c2n co) as x.
+      replace 24%nat with (8 * 3)%nat by trivial.
+      rewrite <- Nat.mul_add_distr_l.
+      admit.
+    - (* long *)
+      unfold content_nblen, nbs_nblen.
+      remember (c2n co) as x.
+      replace 24%nat with (8 * 3)%nat by trivial.
+      rewrite <- Nat.mul_add_distr_l.
+      admit.
+  Admitted.
+
+End Lengths.
   
+
+Section Join.
+
+    
   Definition mk_info (b : BER_nbs) : container info_nblen :=
     match b with
     | short_nbs id co t s bb ff ee e m _ _ _ _ =>
@@ -338,6 +402,29 @@ Section normal.
          )))))
     end.
 
+  Definition append_info {l : nat} (b : BER_nbs) (c : container l) :=
+    join_cont (mk_info b) c.
+  
+  Definition mk_content (b : BER_nbs) : container (content_nblen b) :=
+    match b with
+    | short_nbs id co t s bb ff ee e m _ _ _ _ =>
+      cast_cont (join_cont e m) (short_nblen_correct co ee)
+    | long_nbs id co t s bb ff ee eo e m _ _ _ _ =>
+      cast_cont (join_cont eo (join_cont e m)) (long_nblen_correct co eo)
+    end.
+
+  Definition cont_of_nbs (b : BER_nbs) : container (nbs_nblen b) :=
+    cast_cont (append_info b (mk_content b)) (nbs_nblen_correct b).
+
+End Join.
+
+
+Section Split.
+
+  Definition cut_info {l : nat} (c : container (info_nblen + l)) :
+    (container info_nblen * container l) :=
+    split_cont c.
+
   Definition split_info (c : container info_nblen) :
     (cont8 * cont8 * cont1 * cont1 * cont2 * cont2 * cont2) :=
     let '(id, c) := cut_b8_cont c in
@@ -347,6 +434,67 @@ Section normal.
     let '(bb, c) := cut_b2_cont c in
     let '(ff, ee) := cut_b2_cont c in
     (id, co, t, s, bb, ff, ee).
+
+  Definition split_cut_info {l : nat} (c : container (info_nblen + l)) :=
+    let '(info, content) := cut_info c in
+    let '(id, co, t, s, bb, ff, ee) := split_info info in
+    (id, co, t, s, bb, ff, ee, content).
+
+  Definition e_blen_of_long_cont {l : nat} (c : container (8 + l)) : nat :=
+    c2n (fst (cut_b8_cont c)).
+
+  Definition cut_num (a b : nat) : option (a = b + (a - b))%nat :=
+    match (Nat.eq_dec a (b + (a - b))) with
+    | right _ => None
+    | left H => Some H
+    end.
+
+  Definition cut_cont {l : nat} (c : container l) (l1 : nat) : option (container (l1 + (l - l1))) :=
+    match (cut_num l l1) with
+    | None => None
+    | Some H => Some (cast_cont c H)
+    end.
+
+  Definition split_long_cont  {l : nat} (c : container (8 + l))
+    : option (container 8 *
+              container (e_blen_of_long_cont c) *
+              container (l - e_blen_of_long_cont c))
+    :=
+      let eo := fst (cut_b8_cont c) in
+      let em := snd (cut_b8_cont c) in
+      let eon := z2n (Z_of_cont eo) in
+      match (cut_cont em eon) with
+      | None => None
+      | Some em =>
+      let '(e, m) := split_cont em in
+        Some (eo, e, m)
+      end.
+
+  (**********************************************************************************)
+  (* Definition cont_len_split {l : nat} (c : container l) (l1 : nat) :=            *)
+  (*   match (cut_cont c info_nblen) with                                           *)
+  (*   | None => None                                                               *)
+  (*   | Some c =>                                                                  *)
+  (*     let '(id, co, t, s, bb, ff, ee, content) := split_cut_info c in            *)
+  (*     if (c2n ee =? 3)%nat                                                       *)
+  (*     then match (cut_cont content 8) with                                       *)
+  (*          | None => None                                                        *)
+  (*          | Some content =>                                                     *)
+  (*            match (split_long_cont content) with                                *)
+  (*            | None => None                                                      *)
+  (*            | Some (eo, e, m) =>                                                *)
+  (*              match (valid_long_nbs_sb id, co, t, s, bb, ff, ee, eo, e, m) with *)
+  (*                |                                                               *)
+  (*            end                                                                 *)
+  (*          end                                                                   *)
+  (*     else match (cut_cont content (c2n ee + 1)) with                            *)
+  (*          | None => None                                                        *)
+  (*          | Some c =>                                                           *)
+  (*            let '(e, m) := split_cont c in                                      *)
+  (*            (id, co, t, s, bb, ff, ee, e, m)                                    *)
+  (*          end                                                                   *)
+  (*   end.                                                                         *)
+  (**********************************************************************************)
 
   Lemma split_mk_info (b : BER_nbs) :
     match b with
@@ -373,66 +521,14 @@ Section normal.
     rewrite split_join_roundtrip in Heqp4; tuple_inversion.
     reflexivity.
   Qed.
-  
-  Definition cut_info {l : nat} (c : container (info_nblen + l)) :
-    (container info_nblen * container l) :=
-  split_cont c.
-    
-  Definition append_info {l : nat} (b : BER_nbs) (c : container l) :=
-    join_cont (mk_info b) c.
 
-  Definition content_nblen (b : BER_nbs) : nat :=
-    match b with
-    | short_nbs id co t s bb ff ee e m _ _ _ _ => 8*(c2n co - 1)
-    | long_nbs id co t s bb ff ee eo e m _ _ _ _ => 8*(c2n co - 1)
-    end.
+End Split.
 
-  Definition nbs_nblen (b : BER_nbs) : nat :=
-    match b with
-    | short_nbs id co t s bb ff ee e m _ _ _ _ => 8*(c2n co + 2)
-    | long_nbs id co t s bb ff ee eo e m _ _ _ _ => 8*(c2n co + 2)
-    end.
-
-  Program Definition mk_content (b : BER_nbs) : container (content_nblen b) :=
-    match b with
-    | short_nbs id co t s bb ff ee e m _ _ _ _ =>
-      join_cont e m
-    | long_nbs id co t s bb ff ee eo e m _ _ _ _ =>
-      join_cont eo (join_cont e m)
-    end.
-
-  Next Obligation.
-  Admitted.
-  Next Obligation.
-  Admitted.
-
-
-  (* splitting *)
-
-  Definition e_blen_of_long_cont {l : nat} (c : container (8 + l)) : nat :=
-    z2n (Z_of_cont (fst (cut_b8_cont c))).
-
-  Definition split_long_cont  {l : nat} (c : container (8 + l))
-    : option (container 8 *
-              container (e_blen_of_long_cont c) *
-              container (l - e_blen_of_long_cont c))
-    :=
-      let eo := fst (cut_b8_cont c) in
-      let em := snd (cut_b8_cont c) in
-      let eon := z2n (Z_of_cont eo) in
-      match (Nat.eq_dec l (eon + (l - eon))) with
-      | right _ => None
-      | left H =>
-      let '(e, m) := split_cont (cast_cont em H) in
-        Some (eo, e, m)
-      end.
-
-End normal.
 
 Section special.
-  
 
 End special.
+
 
 Definition BER_blen (b : Z) : nat :=
   let l := nblen b in
@@ -447,13 +543,6 @@ Definition cont_of_BER_bits (b : Z) : option (container (BER_blen b)) :=
   | left _ => None
   | right N => Some (cont (BER_blen b) b N (BER_blen_correct b))
   end.
-
-(*
-Lemma cut_append_info {l : nat} (b : BER_bitstring) (c : container l) :
-    cut_info (append_info b c) = (mk_info b, c).
-
-Definition cut_id {l : nat} (c : container (8 + l)) :=
-*)
 
 Definition bitstring_of_bits (b : Z) : option BER_bitstring :=
   match classify_BER b with
@@ -488,5 +577,3 @@ Definition bitstring_of_bits (b : Z) : option BER_bitstring :=
 
 Definition bits_of_bitstring (b : BER_bitstring) : Z.
 Admitted.
-
-End outer_sec.
