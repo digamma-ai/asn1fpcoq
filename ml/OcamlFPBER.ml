74946a6eb4d80b57d3f3cc6bca5f2755dd219f7d
Ilia Zaichuk <zoickx@gmail.com>
2019-02-12 03:15:15 +0200

    Add binary printing for big_int

--- ml/OcamlFPBER.ml
+++ ml/OcamlFPBER.ml
@@ -3,13 +3,41 @@ open Core
 open String
 open Big_int
 
+let string_of_big_int_bit (x : big_int) : string =
+  if eq_big_int x zero_big_int
+  then "0"
+  else "1"
+
+let big_int_bit_of_string (x : string) : big_int =
+  if x = "0"
+  then zero_big_int
+  else unit_big_int
+
+let rec bits_of_big_int bi =
+  let base = big_int_of_int 2 in
+  let (q,r) = quomod_big_int bi base in
+  let t = string_of_big_int_bit r in
+  String.concat [(if eq_big_int q zero_big_int then "" else bits_of_big_int q); t]
+
+  (*
 let rec bytes_of_big_int bi =
   let base = big_int_of_int 256 in
   let (q,r) = quomod_big_int bi base in
   let t = char_of_int (int_of_big_int r) in
   (if eq_big_int q zero_big_int then "" else bytes_of_big_int q)
   ^ of_char t
+   *)
 
+let rec big_int_of_bits ?acc:(a=big_int_of_int 0) s =
+  let base = big_int_of_int 2 in
+  if is_empty s then a
+  else
+    let x = get s 0 in
+    let xs = drop_prefix s 1 in
+    let v = (big_int_bit_of_string (String.of_char x)) in
+    big_int_of_bits ~acc:((add_big_int (mult_big_int a base) v)) xs
+
+    (*
 let rec big_int_of_bytes ?acc:(a=big_int_of_int 0) s =
   let base = big_int_of_int 256 in
   if is_empty s then a
@@ -18,6 +46,7 @@ let rec big_int_of_bytes ?acc:(a=big_int_of_int 0) s =
     let xs = drop_prefix s 1 in
     let v = big_int_of_int (int_of_char x) in
     big_int_of_bytes ~acc:((add_big_int (mult_big_int a base) v)) xs
+     *)
 
 let unsigned_big_int_of_int64 (i:int64) =
   let open Core.Int64 in
@@ -45,16 +74,16 @@ let ocaml_float64_to_BER_exact (radix:big_int) (scaled:bool) (f:float): String.t
   let fb = big_int_of_float f in
   match float64_to_BER_exact radix scaled fb with
   | None -> None
-  | Some bbi -> Some (bytes_of_big_int bbi)
+  | Some bbi -> Some (bits_of_big_int bbi)
 
 let ocaml_BER_to_float64_exact (ab:string): float option =
-  let ai = big_int_of_bytes ab in
+  let ai = big_int_of_bits ab in
   match coq_BER_to_float64_exact ai with
   | None -> None
   | Some fbi -> Some (float_of_big_int fbi)
 
 let ocaml_BER_to_float64_rounded (m:Binary.mode) (ab:string): float option =
-  let ai = big_int_of_bytes ab in
+  let ai = big_int_of_bits ab in
   match coq_BER_to_float64_rounded m ai with
   | None -> None
   | Some fbi -> Some (float_of_big_int fbi)
