ac18a0adaba9b1fda48a6da282ada4b9a386e16c
yarik <yroslav541@gmail.com>
2019-03-04 18:06:20 +0200

    Build object file throug dune; Not working

--- ml/OcamlWrappers.ml
+++ ml/OcamlWrappers.ml
@@ -4,7 +4,6 @@ open Big_int
 let ocaml_float64_to_BER (f: float): string option = 
   ocaml_float64_to_BER_exact (big_int_of_int 2) false f
 
-let () =
-  Callback.register "ocaml_double_to_ber" ocaml_float64_to_BER;
-  Callback.register "ocaml_ber_to_double" ocaml_BER_to_float64_exact;
-;;
+let () = Printf.printf "This location was reached\n%!"
+let () = Callback.register "double_to_ber" ocaml_float64_to_BER
+let () = Callback.register "ber_to_double" ocaml_BER_to_float64_exact
