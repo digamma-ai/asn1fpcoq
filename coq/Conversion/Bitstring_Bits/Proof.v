0d8ae26acda09003d6b636d76aa72c3f46660249
zoickx <zoickx@ztld.org>
2019-04-22 23:32:17 +0300

    Add structural proof of `bsaux_bits_roundtrip`
    Proof is, for the most part, complete, but relies on admitted lemmas

--- coq/Conversion/Bitstring_Bits/Proof.v
+++ coq/Conversion/Bitstring_Bits/Proof.v
@@ -296,6 +296,11 @@ Proof.
   all: reflexivity.
 Qed.
 
+
+Theorem nbs_cont_roundtrip (b : BER_nbs) :
+  eq_lift2 BER_nbs_eqb (nbs_of_cont (cont_of_nbs b)) (Some b) true.
+Admitted.
+
 Theorem bsaux_bits_roundtrip (b : BER_bs_aux) :
   roundtrip_option
       BER_bs_aux Z BER_bs_aux
@@ -321,9 +326,7 @@ Proof.
       unfold bsaux_nblen.
       rewrite nbs_is_not_special.
       intros H; inversion H.
-  - remember (cont_of_bsaux b) as cont_of_b.
-    remember (c2z cont_of_b) as bits_of_b.
-    unfold BER_bsaux_eqb.
+  - unfold BER_bsaux_eqb.
     break_match; subst b.
 
     + (* was special, decoded as nbs *)
@@ -333,150 +336,35 @@ Proof.
       simpl.
       repeat break_match; discriminate.
 
-
-
-      
     + (* was nbs, decoded as nbs *)
       rename b0 into b.
       rename b1 into r_b.
 
       (** * care: might be losing information *)
       clear Heqo0 Heqb1.
-      
-      (* clean simplification *)
-      unfold bsaux_nblen in cont_of_b;
-        unfold cont_of_bsaux in Heqcont_of_b.
 
-      destruct b, r_b; simpl; apply (f_equal Some).
-      remember (short_nbs id co t s bb ff ee e m VS1 VS2 VS3 VS4 VS5) as b.
+      unfold cont_of_bsaux in *.
+      unfold cont_of_BER_bits in Heqo1.
+      break_match; inversion Heqo1; subst c; clear Heqo1.
+      generalize (cont_of_bits_of_cont_of_nbs b l); intros H.
+      generalize (nbs_of_cont_inj
+                 (cont_of_nbs b)
+                 (cont (BER_nblen (c2z (cont_of_nbs b)))
+                       (c2z (cont_of_nbs b))
+                       l
+                       (BER_nblen_correct (c2z (cont_of_nbs b)))) H);
+        intros H1.
+      unfold bsaux_nblen in *.
+      rewrite Heqo2 in H1.
+      unfold eq_lift2 in H1; break_match; inversion H1; clear H2.
+      generalize (nbs_cont_roundtrip b); intros H2.
 
-      rename id0 into r_id;  move r_id  after b.
-      rename co0 into r_co;  move r_co  after b.
-      rename  t0 into r_t;   move r_t   after b.
-      rename  s0 into r_s;   move r_s   after b.
-      rename bb0 into r_bb;  move r_bb  after b.
-      rename ff0 into r_ff;  move r_ff  after b.
-      rename ee0 into r_ee;  move r_ee  after b.
-      rename  e0 into r_e;   move r_e   after b.
-      rename  m0 into r_m;   move r_m   after b.
-      rename VS0 into r_VS1; move r_VS1 after b.
-      rename VS6 into r_VS2; move r_VS2 after b.
-      rename VS7 into r_VS3; move r_VS3 after b.
-      rename VS8 into r_VS4; move r_VS4 after b.
-      rename VS9 into r_VS5; move r_VS5 after b.
-
-
-      rename Heqb into B, Heqcont_of_b into COB, Heqbits_of_b into BOB.
-      move BOB at top.
-      move COB at top.
-      move B at top.
-      rename c into r_cont_of_b, Heqo1 into r_COB, Heqo2 into r_B.
-
-
-
-
-
-      * (* was short, decoded as short *)
-        unfold cont_of_BER_bits in r_COB; break_match; inversion r_COB.
-        subst r_cont_of_b; clear r_COB.
-        unfold nbs_of_cont, construct_long_nbs, construct_short_nbs in r_B.
-        repeat break_match; inversion r_B.
-        subst.
-        apply ProofIrrelevance.ProofIrrelevanceTheory.EqdepTheory.inj_pair2 in H7.
-        repeat apply ProofIrrelevance.ProofIrrelevanceTheory.EqdepTheory.inj_pair2 in H8.
-        subst.
-        clear r_B.
-        admit.
-
-
-      * (* was short, decoded as long *)
-        exfalso.
-        unfold cont_of_BER_bits in Heqo1;
-          break_match; inversion Heqo1; clear Heqo1.
-        subst c.
-        admit.
-      * (* was long, decoded as short *)
-        (** same as short -> long *)
-        admit.
-      * (* was long, decoded as long *)
-        (** same as short -> short *)
-        admit.
-        
-        
-
-
-
-
-      
-        (*
-      * (* was short, decoded as short *)
-        
-
-        unfold cont_of_BER_bits in Heqo1.
-        break_match; inversion Heqo1; clear Heqo1.
-        subst c.
-        unfold nbs_of_cont in Heqo2.
-        break_match; [| inversion Heqo2].
-        repeat break_match; inversion Heqo2; clear Heqo2.
-        
-        -- (* trying to decode as long nbs *)
-           unfold construct_long_nbs in H0.
-           repeat break_match; inversion H0.
-
-        -- (* decoding as short nbs *)
-           unfold construct_short_nbs in H0.
-           repeat break_match; inversion H0; clear H0.
-           
-           subst.
-           apply ProofIrrelevance.ProofIrrelevanceTheory.EqdepTheory.inj_pair2 in H8.
-           repeat apply ProofIrrelevance.ProofIrrelevanceTheory.EqdepTheory.inj_pair2 in H9.
-
-           unfold try_cut_cont in Heqo; break_match; inversion Heqo; clear Heqo.
-           break_match; inversion H0; clear H0.
-           unfold split_cut_info, cut_info,
-             split_cont, cut_cont, cast_cont, cut_num, try_cut_num in Heqp.
-           repeat break_match.
-           inversion Heqp0; clear Heqp0.
-
-           unfold cut_cont in H1.
-           unfold cast_cont in H1.
-
-
-
-           
-
-           destruct id as (id, IDN, IDL).
-           destruct co as (co, CON, COL).
-           destruct  t as ( t,  TN,  TL).
-           destruct  s as ( s,  SN,  SL).
-           destruct bb as (bb, BBN, BBL).
-           destruct ff as (ff, FFN, FFL).
-           destruct ee as (ee, EEN, EEL).
-           destruct e  as ( e,  EN,  EL).
-           destruct m  as ( m,  MN,  ML).
-
-           destruct id0 as (id0, IDN0, IDL0).
-           destruct co0 as (co0, CON0, COL0).
-           destruct  t0 as ( t0,  TN0,  TL0).
-           destruct  s0 as ( s0,  SN0,  SL0).
-           destruct bb0 as (bb0, BBN0, BBL0).
-           destruct ff0 as (ff0, FFN0, FFL0).
-           destruct ee0 as (ee0, EEN0, EEL0).
-           destruct e0  as ( e0,  EN0,  EL0).
-           destruct m0  as ( m0,  MN0,  ML0).
-
-           unfold c2z, Z_of_cont in *.
-           subst.
-           split_andb_goal.
-           simpl.
-
-
-           unfold cont_eqb.
-
-
-           subst.
-*)
-
+      unfold eq_lift2 in H2; break_match; inversion H2; clear H3.
+      rewrite H1.
+      rewrite BER_nbs_eqb_trans with (b2 := b0).
+      reflexivity.
+      inversion Heqo; rewrite <-H3; rewrite BER_nbs_eqb_sym; assumption.
+      assumption.
   - admit. (* trivial *)
   - admit.
   - admit. (* trivial *)
