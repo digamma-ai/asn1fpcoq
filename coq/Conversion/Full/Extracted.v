bc76a2c80b6f7d6fa533dc416851931b5f78d075
zoickx <zoickx@ztld.org>
2019-06-02 14:33:52 +0300

    Use `binary_float` instead of `Z` to represent IEEE
    IEEE -> ASN conversion now starts "later":
    binary_float is expected instead of a bit string (Z)
    
    ASN -> IEEE conversion now ends "earlier":
    binary_float is returned instead of a bit string (Z)
    
    THIS BREAKS OUNIT TESTS:
    Tests in OCaml are built for functions
    converting bit strings to bit strings.
    To fix: change ocaml code to feed `binary_float`s into
    tested conversion functions.

--- coq/Conversion/Full/Extracted.v
+++ coq/Conversion/Full/Extracted.v
@@ -25,38 +25,34 @@ Search (Z -> Z).
 (* extraction-ready functions for the most common IEEE formats *)
 Section B32.
 
-  Definition float32_to_BER_exact (target_radix : radix) (scaled : bool) : Z -> option Z :=
+  Definition float32_to_BER_exact (target_radix : radix) (scaled : bool)
+  : binary_float 24 128 -> option Z :=
     ret ∘ bits_of_bsaux ∘ bsaux_of_bitstring <=< bitstring_of_BER <=<
-        BER_of_b32_abstract ∘ b32_of_bits.
+        BER_of_b32_abstract.
 
-  Definition BER_to_float32_exact : Z -> option Z :=
-    ret ∘ bits_of_b32 <=< b32_of_BER_abstract_exact <=<
+  Definition BER_to_float32_exact : Z -> option (binary_float 24 128) :=
+    b32_of_BER_abstract_exact <=<
         BER_of_bitstring ∘ bitstring_of_bsaux <=< bsaux_of_bits.
 
-  Definition BER_to_float32_rounded (rounding: mode) : Z -> option Z :=
-    ret ∘ bits_of_b32 <=< (b32_of_BER_abstract_rounded rounding) <=<
+  Definition BER_to_float32_rounded (rounding: mode) : Z -> option (binary_float 24 128) :=
+    (b32_of_BER_abstract_rounded rounding) <=<
         BER_of_bitstring ∘ bitstring_of_bsaux <=< bsaux_of_bits.
-
-  Definition normalize_float32 (m e : Z) : Z * Z :=
-    normalize_b32_abstract m e.
 
 End B32.
 
 Section B64.
 
-  Definition float64_to_BER_exact (target_radix : radix) (scaled : bool) : Z -> option Z :=
+  Definition float64_to_BER_exact (target_radix : radix) (scaled : bool)
+  : binary_float 53 1024 -> option Z :=
     ret ∘ bits_of_bsaux ∘ bsaux_of_bitstring <=< bitstring_of_BER <=<
-        BER_of_b64_abstract ∘ b64_of_bits.
+        BER_of_b64_abstract.
 
-  Definition BER_to_float64_exact : Z -> option Z :=
-    ret ∘ bits_of_b64 <=< b64_of_BER_abstract_exact <=<
+  Definition BER_to_float64_exact : Z -> option (binary_float 53 1024) :=
+    b64_of_BER_abstract_exact <=<
         BER_of_bitstring ∘ bitstring_of_bsaux <=< bsaux_of_bits.
 
-  Definition BER_to_float64_rounded (rounding: mode) : Z -> option Z :=
-    ret ∘ bits_of_b64 <=< (b64_of_BER_abstract_rounded rounding) <=<
+  Definition BER_to_float64_rounded (rounding: mode) : Z -> option (binary_float 53 1024) :=
+    (b64_of_BER_abstract_rounded rounding) <=<
         BER_of_bitstring ∘ bitstring_of_bsaux <=< bsaux_of_bits.
-
-  Definition normalize_float64 (m e : Z) : Z * Z :=
-    normalize_b64_abstract m e.
 
 End B64.
