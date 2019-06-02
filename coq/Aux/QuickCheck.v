ff34bfe2baa55764f35e66ee9ca04feb779854b2
zoickx <zoickx@ztld.org>
2019-06-02 14:44:45 +0300

    Fix QuickChick tests to check the Full converion
    Original QuickChick test only tested one step of the conversion -
    abstract IEEE_float <-> abstract BER_float,
    omitting all intermediate steps: BER_bitstring, BER_bs_aux, BER bits
    
    The test is now performed on the full conversions, using full
    extraction-ready functions.

--- coq/Aux/QuickCheck.v
+++ coq/Aux/QuickCheck.v
@@ -1,15 +1,14 @@
-Require Import ZArith PArith Lia.
+Require Import ZArith PArith Lia List String.
 Require Import Flocq.IEEE754.Binary Flocq.Core.Zaux Flocq.IEEE754.Bits.
 Require Import ASN1FP.Aux.Roundtrip ASN1FP.Conversion.Full.Extracted.
 Require Import ASN1FP.Conversion.IEEE_ASN.
-Require Import ASN1FP.Aux.Flocq.
 
 From QuickChick Require Import QuickChick.
 Set Warnings "-extraction-opaque-accessed,-extraction".
 
 Open Scope Z.
 
-Require Import String. Open Scope string.
+Open Scope string.
 
 Instance show_binary : Show (binary_float 24 128) := {
   show c := match c with
@@ -84,31 +83,17 @@ Definition binary_gen (t : G bool) : G (option (binary_float 24 128)) :=
 
 Definition fing := binary_gen (choose (true, false)).
 
-Require Import List.
 Import ListNotations.
 
 Definition binary32_gen : G (option (binary_float 24 128)) :=
   freq_ zerg [(1, zerg)%nat ; (1, infg)%nat ; (1, nang)%nat ; (7, fing)%nat].
 
-Definition binary_float_eqb {prec emax : Z} (a1 a2 : binary_float prec emax) :=
-  match a1, a2 with
-  | B754_nan _ _ _, B754_nan _ _ _ => true
-  | _,_ =>
-    match Bcompare prec emax a1 a2 with
-    | Some cmp => match cmp with
-                 | Eq => true
-                 | _ => false
-                 end
-    | None => false
-    end
-  end.
-
 Definition binary_float_BER_exact_roundtrip (b32 : binary_float 24 128) : bool
   := roundtrip_bool
-       (binary_float 24 128) ASN.BER_float (binary_float 24 128)
-       (BER_of_IEEE_exact 24 128)
-       Abstract.b32_of_BER_abstract_exact
-       binary_float_eqb
+       (binary_float 24 128) Z (binary_float 24 128)
+       (float32_to_BER_exact radix2 false)
+       BER_to_float32_exact
+       float_eqb_nan_t
        b32.
 
 QuickChick (forAllMaybe binary32_gen binary_float_BER_exact_roundtrip).
