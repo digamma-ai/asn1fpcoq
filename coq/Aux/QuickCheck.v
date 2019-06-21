db2717ee94784772e706a87aeb50f885528c973d
yarik <yroslav541@gmail.com>
2019-06-21 20:07:23 +0300

    Incomplete proof of finite sub-generator

--- coq/Aux/QuickCheck.v
+++ coq/Aux/QuickCheck.v
@@ -4,6 +4,8 @@ Require Import ASN1FP.Aux.Roundtrip ASN1FP.Conversion.Full.Extracted.
 Require Import ASN1FP.Conversion.IEEE_ASN.
 Require Import ASN1FP.Aux.Flocq.
 Require Import ASN1FP.Aux.StructTactics.
+Import ListNotations.
+
 From QuickChick Require Import QuickChick.
 Set Warnings "-extraction-opaque-accessed,-extraction".
 
@@ -13,15 +15,14 @@ Open Scope string.
 
 Instance show_binary : forall (prec emax : Z), Show (binary_float prec emax) := {
   show c := match c with
-              | B754_zero false => "+zero"
-              | B754_zero true => "-zero"
+              | B754_zero false => "+0"
+              | B754_zero true => "-0"
               | B754_infinity false => "+inf"
               | B754_infinity true => "-inf"
-              | B754_nan false _ _ => "+nan"
-              | B754_nan true _ _ => "-nan"
-              | B754_finite s m e _ => (if s then "+" else "-")
-                                        ++ (show_Z (Z.pos m))
-                                        ++ (show_Z e)
+              | B754_nan false _ _ => "+NaN"
+              | B754_nan true _ _ => "-NaN"
+              | B754_finite s m e _ => (if s then "" else "-")
+                                        ++ (show_Z (Z.pos m * 2 ^ e))
             end
 }.
 
@@ -33,15 +34,15 @@ Definition binary64 := binary_float 53 1024.
 Definition zerg (prec emax : Z) := 
   (liftGen (fun (s : bool) => B754_zero prec emax s)) 
     (choose (true, false)).
+
 Definition infg (prec emax : Z) := 
   (liftGen (fun (s : bool) => B754_infinity prec emax s))
     (choose (true, false)).
 
-Lemma nan1 : nan_pl 24 1%positive = true.
-Proof. auto. Qed.
-
-Definition nang (prec emax : Z) (np : nan_pl prec 1 = true) :=
-  (liftGen (fun b => B754_nan prec emax b 1%positive np))
+Definition nang (prec emax : Z) 
+        (pl : positive) 
+        (np : nan_pl prec pl = true) := 
+  (liftGen (fun b => B754_nan prec emax b pl np))
     (choose (true, false)).
 
 Definition boundaries (prec emax : Z) (t : bool) :=
@@ -49,7 +50,9 @@ Definition boundaries (prec emax : Z) (t : bool) :=
       then (1, 2^prec - 1, 3 - emax - prec, 3 - emax - prec)%Z 
       else (2^(prec - 1), 2^prec - 1, 3 - emax - prec, emax - prec)%Z.
 
-Program Definition fing (prec emax : Z) : G (binary_float prec emax) :=
+Program Definition fing (prec emax : Z) 
+        (prec_gt_0 : Flocq.Core.FLX.Prec_gt_0 prec)
+        (Hmax : (prec < emax)%Z) : G (binary_float prec emax) :=
   bindGen' (choose (false, true)) (fun t => fun b0 => 
     bindGen' (returnGen (boundaries prec emax t)) 
     (fun '(m_min, m_max, e_min, e_max) => fun b1 =>
@@ -59,7 +62,7 @@ Program Definition fing (prec emax : Z) : G (binary_float prec emax) :=
             returnGen (B754_finite prec emax s (Z.to_pos m) e _)))))).
 Next Obligation.
   (* get rid of technical junk *)
-  (* clear s b0 b2; rename b3 into b2, b4 into b3.
+  clear s b0 b2; rename b3 into b2, b4 into b3.
   apply semReturn in b1.
   apply semChoose in b2.
   apply semChoose in b3.
@@ -69,13 +72,33 @@ Next Obligation.
   apply andb_prop in b2; destruct b2 as [B11 B12].
   apply andb_prop in b3; destruct b3 as [B21 B22].
   all: destruct t; try rewrite Z.leb_le in *.
+  Opaque Z.sub.
   all: tuple_inversion.
+  all: try unfold FLX.Prec_gt_0 in *.
 
   (* solve easy subgoals *)
   - admit.
   - admit.
-  3,4,5: lia.
-  3: rewrite Z.pow_pos_fold; lia.
+  - reflexivity.
+  - lia.
+  - 
+    assert (T : (1) = (2 - 1)) by lia; rewrite T; clear T.
+    rewrite <-Z.sub_le_mono_r.
+    assert (T : (2) = (2 ^ 1)) by lia; rewrite T; clear T.
+    apply Z.pow_le_mono_r; lia; lia.
+  -
+    clear m b2 e b3 H Hmax emax.
+    apply Z.bi_induction with (P := fun prec => 2 ^ (prec - 1) <= 2 ^ prec - 1).
+    unfold Morphisms.Proper, Morphisms.respectful, Z.eq.
+    intros.
+    rewrite H.
+    lia.
+    reflexivity.
+    intros.
+    unfold Z.succ.
+    assert (T : x + 1 - 1 = x) by lia; rewrite T; clear T.
+    split.
+    intros.
 
   (* simplify for main subgoals *)
   all: assert (T : m < 16777216) by lia; clear B12; rename T into B12.
@@ -98,15 +121,19 @@ Next Obligation.
   destruct m; try lia.
   rewrite Zlog2_log_inf.
   reflexivity.
-Qed. *)
+Qed.
 Admitted.
 
-Import ListNotations.
+Theorem fing32_prec : FLX.Prec_gt_0 24.
+Proof. unfold FLX.Prec_gt_0; reflexivity. Qed.
+
+Theorem fing32_prec_emax : 24 < 128.
+Proof. reflexivity. Qed.
 
 Definition zerg32 := zerg 24 128.
 Definition infg32 := infg 24 128.
-Definition nang32 := nang 24 128 nan1.
-Definition fing32 := fing 24 128.
+Program Definition nang32 := nang 24 128 1 _.
+Definition fing32 := fing 24 128 fing32_prec fing32_prec_emax.
 
 Definition binary32_gen : G (binary32) :=
   freq_ zerg32 [(1, zerg32)%nat ; (1, infg32)%nat ;
@@ -122,10 +149,16 @@ Definition binary_float32_BER_exact_roundtrip (b32 : binary32) : bool
 
 QuickChick (forAll binary32_gen binary_float32_BER_exact_roundtrip).
 
+Theorem fing64_prec : FLX.Prec_gt_0 53.
+Proof. unfold FLX.Prec_gt_0; reflexivity. Qed.
+
+Theorem fing64_prec_emax : 53 < 1024.
+Proof. reflexivity. Qed.
+
 Definition zerg64 := zerg 53 1024.
 Definition infg64 := infg 53 1024.
-Definition nang64 := nang 53 1024 nan1.
-Definition fing64 := fing 53 1024.
+Program Definition nang64 := nang 53 1024 1 _.
+Definition fing64 := fing 53 1024 fing64_prec fing64_prec_emax.
 
 Definition binary64_gen : G (binary64) :=
   freq_ zerg64 [(1, zerg64)%nat ; (1, infg64)%nat ;
